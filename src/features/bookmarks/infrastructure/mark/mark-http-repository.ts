62315d8e5f830ea49cd3ea465b3298f9322d6996
iruizdorronsoro <iruizdorronsoro@gmail.com>
2021-09-30 21:38:18 +0200

    try changing findall return: add folder mapper x4

--- src/features/bookmarks/infrastructure/mark/mark-http-repository.ts
+++ src/features/bookmarks/infrastructure/mark/mark-http-repository.ts
@@ -9,17 +9,21 @@ import {CreateFolderDto} from "../folder/create-folder-dto";
 import {FolderToEditFolderDtoMapper} from "../folder/folder-to-edit-folder-dto-mapper";
 import {EditFolderDto} from "../folder/edit-folder-dto";
 import {AxiosResponse} from "axios";
+import {FolderDtoToFolderMapper} from "../folder/folder-dto-to-folder-mapper";
+import {FolderDto} from "../folder/folder-dto";
 //TODO: Promise response
 export class MarkHttpRepository implements MarkRepository {
   constructor(
     private readonly markDtoToMarkMapper: MarkDtoToMarkMapper,
     private readonly markToEditMarkDtoMapper: MarkToEditMarkDtoMapper,
-    private readonly folderToEditFolderDtoMapper: FolderToEditFolderDtoMapper
+    private readonly folderToEditFolderDtoMapper: FolderToEditFolderDtoMapper,
+    private readonly folderDtoToFolderMapper: FolderDtoToFolderMapper
   ) {}
 
   async findAll(): Promise<any> {
     const response: AxiosResponse = await http.get('/home', {baseURL: "https://laster-marka-back.herokuapp.com"})
-    return response.data
+    const folders: Folder[] = response.data.folders.map((folder: FolderDto) => {return this.folderDtoToFolderMapper.map(folder)})
+    return folders
   }
 
   async create(createMarkDto: CreateMarkDto, idFolder: string): Promise<any> {
