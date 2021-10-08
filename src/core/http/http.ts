f33c1d3fc61a8cbb83e1f664c070b0705d99c7c4
iruizdorronsoro <iruizdorronsoro@gmail.com>
2021-10-08 20:49:27 +0200

    header cookie test

--- src/core/http/http.ts
+++ src/core/http/http.ts
@@ -2,5 +2,8 @@ import axios from 'axios'
 
 export const http = axios.create({
   baseURL: 'https://laster-marka-back.herokuapp.com',
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    Cookie: "jwt=value"
+  }
 })
