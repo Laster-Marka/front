24eea8d2b7e5ddc8b46d2ae18e2746c2584b6a53
iruizdorronsoro <iruizdorronsoro@gmail.com>
2021-10-14 18:20:38 +0200

    build db marks

--- src/features/bookmarks/ui/marks/marks.tsx
+++ src/features/bookmarks/ui/marks/marks.tsx
@@ -24,7 +24,7 @@ interface Props {
 
 export const Marks: FC<Props> = () => {
 
-  const markRepository = MarkRepositoryFactory.buildLocal()
+  const markRepository = MarkRepositoryFactory.build()
   // const [marks, setMarks] = useState<MarkModel[]>([])
   const [folders, setFolders] = useState<Folder[]>([])
   const [currentFolder, setCurrentFolder] = useState("")
